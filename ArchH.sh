60039a2070d87beb1f9a9b7ce4af50ea821998a6
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2023-03-28 17:47:43 +0200

    Create ArchH.sh

---
+++ ArchH.sh
@@ -0,0 +1,27 @@
+read -p "Type your password for sudo: " password
+User=$(whoami)
+
+echo "$password" | sudo -S pacman -Syyu --noconfirm
+echo "$password" | sudo -S pacman -S base noto-fonts-emoji virtualbox virtualbox-guest-utils  vlc base-devel fontconfig ttf-droid shotwell dolphin binutils linux-headers zsh gcc git make zsh-completions zsh-syntax-highlighting vim --noconfirm
+
+fc-cache
+cd /opt
+echo "$password" | sudo -S git clone https://aur.archlinux.org/yay-git.git
+echo "$password" | sudo -S chown -R $User:$User ./yay-git
+cd /opt/yay-git
+echo "Y" | makepkg -si
+echo "Y" | makepkg -si
+yay -S --noconfirm zsh-theme-powerlevel10k-git sublime-text-4 freedownloadmanager libunity
+
+cd ~
+git clone https://haya123421321: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@github.com/haya123421321/Arch
+echo "$password" | sudo -S cat Arch/zshrc > ~/.zshrc
+echo "$password" | sudo -S cat Arch/p10k.zsh > ~/.p10k.zsh
+mkdir -p ~/.config/fontconfig/conf.d
+cat Arch/1-fonts.conf > ~/.config/fontconfig/conf.d/1-fonts.conf
+echo "$password" | sudo -S cp Arch/1920x1080.jpg /usr/share/wallpapers/Next/contents/images/1920x1080.jpg
+echo 'change shell to zsh'
+printf "$password\n/bin/zsh" | chsh
+echo "$password" | sudo -S rm -R Arch
+
+echo "Taskbar icons: <WebBrowser> Dolphin SystemSettings SystemMonitor" > TaskbarIcons
