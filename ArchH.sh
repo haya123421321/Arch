7f0c4728b1e981c1c470e8ef8d8a094bf5aacf48
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2023-07-30 07:17:20 +0200

    Update ArchH.sh

--- ArchH.sh
+++ ArchH.sh
@@ -26,7 +26,7 @@ echo "Removed BlackArch bootstrap script"
 # Update and install packages from official repositories
 echo "Updating and installing some packages"
 echo "$password" | sudo -S pacman -Syyu --noconfirm > /dev/null 2>&1
-echo "$password" | sudo -S pacman -S git firefox base noto-fonts-emoji gnu-netcat virtualbox dmenu i3 virtualbox-guest-utils sqlitebrowser vlc base-devel fontconfig ttf-droid shotwell dolphin binutils linux-headers whois zsh gcc enum4linux make p7zip zsh-completions zsh-syntax-highlighting openvpn nmap freerdp wireshark-qt aircrack-ng wget gdb vim man sqlmap python2 nikto nfs-utils ruby-irb terminator gobuster binwalk steghide perl-image-exiftool inetutils curlftpfs burpsuite john exploitdb metasploit ffuf hydra hashcat python-pip python2-pip hashid net-tools --noconfirm > /dev/null 2>&1
+echo "$password" | sudo -S pacman -S git firefox base noto-fonts-emoji gnu-netcat virtualbox i3-wm i3status i3lock dmenu virtualbox-guest-utils sqlitebrowser vlc base-devel fontconfig ttf-droid shotwell dolphin binutils linux-headers whois zsh gcc enum4linux make p7zip zsh-completions zsh-syntax-highlighting openvpn nmap freerdp wireshark-qt aircrack-ng wget gdb vim man sqlmap python2 nikto nfs-utils ruby-irb terminator gobuster binwalk steghide perl-image-exiftool inetutils curlftpfs burpsuite john exploitdb metasploit ffuf hydra hashcat python-pip python2-pip hashid net-tools --noconfirm > /dev/null 2>&1
 echo "Updated and installed packages from official repositories"
 
 # Clear font cache
@@ -39,8 +39,16 @@ echo "$password" | sudo -S git clone https://aur.archlinux.org/yay-git.git > /de
 echo "$password" | sudo -S chown -R $User:$User ./yay-git
 cd /opt/yay-git
 yes | makepkg -si > /dev/null 2>&1
-yay -S --noconfirm zsh-theme-powerlevel10k-git sublime-text-4 libunity > /dev/null 2>&1
+yay -S --noconfirm zsh-theme-powerlevel10k-git sublime-text-4 sddm-slice-git libunity > /dev/null 2>&1
 echo "Cloned yay AUR helper and installed some packages from AUR"
+
+# Set SDDM theme to 'slice'
+if [ -f /etc/sddm.conf ]; then
+    sudo sed -i "/^\[Theme\]/,/^\[/ s/^Current=.*/Current=slice/" /etc/sddm.conf
+else
+    echo -e "[Theme]\nCurrent=slice" | sudo tee /etc/sddm.conf > /dev/null 2>&1
+fi
+echo "Set SDDM theme to 'slice'."
 
 # Allow write permissions to certain Python directories
 echo "$password" | sudo -S chmod 777 /usr/lib/python*/site-packages
